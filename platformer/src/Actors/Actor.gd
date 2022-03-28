ba01165773d3d2f89a291eead9e5bb6aac07f388
caiofov <cfoviana@gmail.com>
2022-03-28 17:49:11 -0300

    moving character and jumping physics

--- platformer/src/Actors/Actor.gd
+++ platformer/src/Actors/Actor.gd
@@ -2,6 +2,10 @@ extends KinematicBody2D
 
 #class which both player and enemy will extend
 class_name Actor
+
+const FLOOR_NORMAL: = Vector2.UP #vector that defines the floor (it will be used later)
+#UP -> (0,-1) -> vector pointing up
+
 export var speed: = Vector2(300.0, 1000.0)
 export var gravity: = 3000.0
 #export -> makes the gravity configurable. It will be displayed on the right view inspector
@@ -12,6 +16,6 @@ var velocity: = Vector2.ZERO #velocity in each axis
 #verything that envolves physics: collision etc
 func _physics_process(delta: float) -> void:
 	#delta -> time elapsed since the last frame (the engine will give it to us)
-	velocity.y += gravity * delta
+	pass
 	#velocity.y = max(velocity.y, speed.y) #sets the max velocity
-	velocity = move_and_slide(velocity)
+
--- platformer/src/Actors/Player.gd
+++ platformer/src/Actors/Player.gd
@@ -1,2 +1,40 @@
 extends Actor
 
+#also runs the parent's physics process
+func _physics_process(delta: float) -> void:
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
+	#jump button has just been released and the character is moving up (jumping)
+	#it indentifies if the player has stopped jumping
+	
+	var direction: = get_direction()
+	
+	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL) #2nd parameter -> floor normal: a vector that defines the floor
+	
+	
+func get_direction() -> Vector2:
+	#we have definied those actions in settings
+	#get_action_strength returns a value between 0 and 1 (0 for not pressed at all and 1 for full pressed)
+	return Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0 #if the player has jumped, so it will move up. Otherwise, it will fall
+	)
+	#is_on_floor() -> returns if the character is on the floor
+
+func calculate_move_velocity(
+		linear_velocity: Vector2,
+		direction: Vector2,
+		speed: Vector2,
+		is_jump_interrupted: bool
+	) -> Vector2:
+		var new_velocity: = linear_velocity
+		
+		new_velocity.x = speed.x * direction.x
+		new_velocity.y += gravity * get_physics_process_delta_time() #returns the delta from the physics_process
+		
+		if direction.y == -1.0:
+			new_velocity.y = speed.y * direction.y
+		if is_jump_interrupted:
+			new_velocity.y = 0.0 #it makes the character stop jumping in the middle of a jump when the key is released
+		
+		return new_velocity
