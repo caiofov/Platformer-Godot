4c39a4963a9557e6aef01a18f3d7faada223115a
caiofov <cfoviana@gmail.com>
2022-05-08 16:56:07 -0300

    killing the enemy

--- platformer/src/Actors/Enemy.gd
+++ platformer/src/Actors/Enemy.gd
@@ -3,13 +3,21 @@ extends Actor
 func _ready() -> void: #called once at the beginning
 	set_physics_process(false) #desactivate the enemy until it shows up on the screen
 	_velocity.x = -speed.x #it will start moving left instead of right
+
+#function connected to the stomp detector of the enemy (we added it through the "node" tab of the stomp detector and clicking on area_entered)
+func _on_StompDetector_body_entered(body: Node) -> void:
+	#body -> body which entered in the area (player's body)
+	if body.global_position.y > get_node("StompDetector").global_position.y: #if the player is bellow the enemy
+		return #stops the funcion
+	get_node("CollisionShape2D").disabled = true #disables the collision of the enemy in order to not hit the player
+	queue_free() #kills the enemy - deletes the node
 	
+
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
 	if is_on_wall(): #if the enemy hits a wall, it will change its movement direction
 		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
-	#_velocity.y instead of _velocity because it would change _velocity.x
+	#_velocity.y instead of _velocity because it would change _velocity.x to zero
 	#and the enemy would not change the direction when the wall if hit
-	
-	
+
--- platformer/src/Actors/Player.gd
+++ platformer/src/Actors/Player.gd
@@ -6,7 +6,7 @@ func _physics_process(delta: float) -> void:
 	#jump button has just been released and the character is moving up (jumping)
 	#it indentifies if the player has stopped jumping
 	
-	var direction: = get_direction()
+	var direction: = get_direction() #: -> local variable
 	
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL) #2nd parameter -> floor normal: a vector that defines the floor
@@ -15,6 +15,8 @@ func _physics_process(delta: float) -> void:
 func get_direction() -> Vector2:
 	#we have definied those actions in settings
 	#get_action_strength returns a value between 0 and 1 (0 for not pressed at all and 1 for full pressed)
+	#direction x -> -1 (left) or 1 (right)
+	#direction y -> -1 (up - jumping) or 1 (down - not jumping)
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0 #if the player has jumped, so it will move up. Otherwise, it will fall
