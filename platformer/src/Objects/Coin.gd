02b125fce3247535e5caa6fbe213dfa36e2d3f9b
caiofov <cfoviana@gmail.com>
2022-05-09 17:16:07 -0300

    collecting coins

--- platformer/src/Objects/Coin.gd
+++ platformer/src/Objects/Coin.gd
@@ -1,2 +1,8 @@
 extends Area2D
 
+#onready -> calls the function before the ready function
+onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+
+#indentifies player collision
+func _on_body_entered(body: Node) -> void:
+	anim_player.play("fade_out")
